2aba019f14a79323d88a881a0e50307c852594d7
danielgo98 <daniel.andres1998@gmail.com>
2024-04-04 16:46:51 +0200

    Update: Save and load data from localStorage

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,19 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
-import { Country } from '../../interfaces/country';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
   styleUrl: './by-capital-page.component.css'
 })
-export class ByCapitalPageComponent {
-
-  constructor( private countriesService: CountriesService ) {}
+export class ByCapitalPageComponent implements OnInit {
 
   public countries: Country[] = [];
 
+  public initialValue: string = '';
+
   public isLoadind: boolean = false;
+
+  constructor( private countriesService: CountriesService ) {}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCapital.countries;
+    this.initialValue = this.countriesService.cacheStore.byCapital.term;
+  }
 
   searchByCapital(term: string): void {
 
--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
-import { Country } from '../../interfaces/country';
-
-type Region = 'Africa'| 'Americas'| 'Asia'| 'Europe'| 'Oceania';
+import { Country } from '../../interfaces/country.interface';
+import { Region } from '../../interfaces/region.type';
 
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styleUrl: './by-region-page.component.css'
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
 
   public countries: Country[] = [];
 
@@ -20,6 +19,11 @@ export class ByRegionPageComponent {
   public isLoading: boolean = false;
 
   constructor(private countriesService: CountriesService) {}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
 
   searchByRegion(region: Region): void {
 
