b63bb5f3ea331f9297dc89ddce895e66dbf3c010
danielgo98 <daniel.andres1998@gmail.com>
2024-04-04 14:58:20 +0200

    Update: search countries by region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa'| 'Americas'| 'Asia'| 'Europe'| 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,13 +13,19 @@ export class ByRegionPageComponent {
 
   public countries: Country[] = [];
 
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  public selectedRegion?: Region;
+
   public isLoading: boolean = false;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(region: string): void {
+  searchByRegion(region: Region): void {
 
     this.isLoading = true;
+
+    this.selectedRegion = region;
 
     this.countriesService.searchRegion(region).subscribe(countries => {
       this.countries = countries;
